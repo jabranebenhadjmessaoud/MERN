888648c71dba9d713cb617deb1ebb93381fe2e44
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2024-11-25 18:19:56 +0100

    Conditional Arrows Core

---
+++ week1/Day1/Core/conditional_Arrows.js
@@ -0,0 +1,26 @@
+// Problem 1 
+
+const CheckAge=(age) => age>18 ? "You are good to go !" : "Sorry! You must be 18 or older!"  ;
+
+console.log(CheckAge(19))
+
+
+// Problem 2 
+const checkIfRainy = weather => weather === "rainy" ? "Get your rain jacket!" : "No rain on today's forecast!"
+
+console.log (checkIfRainy("rainy"))
+
+
+// Problem 3 
+
+const CheckEven= number => number%2===0 ? "That's an even number!": "That's an odd number!"
+
+console.log(CheckEven(6))
+
+
+// Problem 4 
+
+const grater = (num1,num2) => num1>num2 ? num1+" is more than "+num2 : num1+" is less than "+num2
+
+console.log(grater(2,3))
+
